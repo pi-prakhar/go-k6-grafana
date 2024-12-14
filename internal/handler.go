477b4c44cfcb74ba40ffe5aef94ba6b53ea17dfa
Prakhar <pawasthi961@gmail.com>
2024-12-14 22:45:11 +0530

    added stress test

--- internal/handler.go
+++ internal/handler.go
@@ -1,6 +1,9 @@
 package api
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+)
 
 type Handler interface {
 	HandleTest(w http.ResponseWriter, r *http.Request)
@@ -17,7 +20,8 @@ func (h *handler) HandleTest(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		http.Error(w, "Bad Request", http.StatusBadRequest)
 	}
-
+	// simulate work
+	time.Sleep(100 * time.Millisecond)
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hello"))
