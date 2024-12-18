250eb8f9878fe4a6d146e9b18ca7318ca8237096
Prakhar <pawasthi961@gmail.com>
2024-12-18 20:43:03 +0530

    added secrets in kubernetes

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,16 +3,24 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	api "github.com/pi-prakhar/go-k6-grafana/internal"
 	"github.com/pi-prakhar/go-k6-grafana/metrics"
 )
 
-const (
-	ADDR string = ":8080"
-)
+// const (
+// 	ADDR string = ":8080"
+// )
+
+func getPort() string {
+	port := os.Getenv("PORT")
+	portString := ":" + port
+	return portString
+}
 
 func main() {
+	ADDR := getPort()
 	metrics.InitMetrics()
 
 	handler := api.NewHandler()
